2fb8b7426a1c6f98477b6f0aa3c2e6eaecb33dd6
borissimkin <simkinbb@gmail.com>
2019-06-05 18:34:01 +0300

    Слежение камеры за персонажем, персонаж следит за мышью
    В данный момент  наблюдается баг, в котором персонаж следит за мышью боком а не передом

---
+++ Assets/Scripts/LookAtMouse.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtMouse : MonoBehaviour {
+
+    public Transform controlThisObject; //Here can be the transform of the weapon
+    private Vector3 mousePos;
+
+    void Update()
+    {
+        MouseL();
+    }
+
+    //Mouse Look script
+    void MouseL()
+    {
+        //Gets mouse position, you can define Z to be in the position you want the weapon to be in
+        mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
+        Vector3 lookPos = Camera.main.ScreenToWorldPoint(mousePos);
+        lookPos = lookPos - transform.position;
+        float angle = Mathf.Atan2(lookPos.y, lookPos.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+}
