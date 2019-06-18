c8e1201b819f488bcc198b57591572c4b3d4e57f
borissimkin <simkinbb@gmail.com>
2019-06-18 08:38:44 +0300

    Реализован спавн врагов

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour {
+    /// <summary>
+    /// здоровье
+    /// </summary>
+    public float health;
+    public float speed;
+    Rigidbody2D rb;
+    public int damage;
+    public int rangeAttack;
+
+    private Transform playerPos;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        playerPos = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>(); //здесь был 
+
+    }
+
+    public void Move()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, playerPos.position, speed * Time.deltaTime);
+    }
+
+    public virtual void Attack()
+    {
+         
+    }
+
+    private void Update()
+    {
+        Move();
+
+    }
+
+
+
+
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+    public GameObject enemy;
+    public Transform[] spawnSpots;
+
+    private float timeBtwSpawns;
+    [SerializeField]
+    private float startTimeBtwSpawns;
+
+
+    private void Start()
+    {
+        timeBtwSpawns = startTimeBtwSpawns;
+    }
+
+    private void Update()
+    {
+        if (timeBtwSpawns <= 0)
+        {
+            int randPos = UnityEngine.Random.Range(0, spawnSpots.Length - 1);
+            Instantiate(enemy, spawnSpots[randPos].position, Quaternion.identity);
+            timeBtwSpawns = startTimeBtwSpawns;
+        }
+        else
+        {
+            timeBtwSpawns -= Time.deltaTime;
+        }
+    }
+
+    private void Instantiate(GameObject enemy, Vector3 position)
+    {
+        throw new NotImplementedException();
+    }
+}
