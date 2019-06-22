770427965284abc58cf33aa57ddbae591c91df40
borissimkin <simkinbb@gmail.com>
2019-06-22 10:20:02 +0300

    Реализован поворот врагов в сторону игрока

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,6 +8,7 @@ public abstract class Enemy : MonoBehaviour {
     /// </summary>
     public float health;
     public float speed;
+    public float speedRotation;
     protected Rigidbody2D rb;
     public int damage;
     public int rangeAttack;
@@ -17,13 +18,22 @@ public abstract class Enemy : MonoBehaviour {
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        playerPos = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>(); //здесь был 
+        playerPos = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>(); 
 
     }
 
     public virtual void Move()
     {
         transform.position = Vector2.MoveTowards(transform.position, playerPos.position, speed * Time.deltaTime);
+    }
+
+
+    public void RotateToPlayer()
+    {
+        Vector2 direction = transform.position - playerPos.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + 90;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, speedRotation * Time.deltaTime);
     }
 
     public virtual void Attack()
--- Assets/Scripts/SimpleEnemy.cs
+++ Assets/Scripts/SimpleEnemy.cs
@@ -13,5 +13,6 @@ public class SimpleEnemy : Enemy {
 	// Update is called once per frame
 	void Update () {
         Move();
+        RotateToPlayer();
 	}
 }
