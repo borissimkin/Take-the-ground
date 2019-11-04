b342882773bf7617c56eca62ecb2c4ab62996a1e
borissimkin <simkinbb@gmail.com>
2019-11-04 21:55:41 +0300

    Реализовал HUD жизней и патронов персонажа

---
+++ Assets/Scripts/HUD/BulletsHUD.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Класс HUD'a патронов текущего оружия в руках персонажа
+/// </summary>
+public class BulletsHUD : MonoBehaviour {
+    [SerializeField]
+    private Text textBullets;
+
+    [SerializeField]
+    private GameObject person;
+
+    private Weapon activeWeapon;
+
+	// Use this for initialization
+	void Start () {
+
+        ShowText();
+	}
+
+	// Update is called once per frame
+	void Update () {
+        ShowText();
+    }
+    void ShowText()
+    {
+        activeWeapon = person.GetComponent<Inventory>().activeWeapon;
+        this.textBullets.text = this.GetTextBullets(activeWeapon);
+    }
+
+    string GetTextBullets(Weapon activeWeapon)
+    {
+        string ammoLeftInClip = activeWeapon.AmmoLeftInClip.ToString();
+        string ammoLeftInStash = activeWeapon.AmmoLeftInStash.ToString();
+        return string.Format("{0} / {1}", ammoLeftInClip, ammoLeftInStash);
+    }
+
+}
